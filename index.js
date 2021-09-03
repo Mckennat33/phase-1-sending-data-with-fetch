4b005e8cce32a28f3b2d6d0c65790280e5a21876
Thomas <thomasmckenna12gmail.com>
2021-09-03 14:34:09 -0400

    phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+const userURL = 'http://localhost:3000/users' 
+
+let bodyData = { 
+    name: "Steve", 
+    email: "steve@steve.com"
+}
+
+const postObject = {
+    method: "POST", 
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(bodyData)
+}
+
+function submitData() {
+    return fetch(userURL, postObject)
+    .then(response => response.json())
+    .then(getID)
+    .catch(errorFunc)
+}
+
+const bodyTag = document.querySelector('body')
+
+function getID(object) {
+    document.body.innerHTML = object.id
+}
+
+function errorFunc(error) {
+    document.body.innerHTML = error.message
+};
